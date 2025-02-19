26eac42ac5ca9d9765505723ae486ad6b6452977
Jagriti Gautam <jagritigautam793@gmail.com>
2025-02-19 20:30:50 +0530

    Tab bar updated

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,28 @@
-import { Text, View } from "react-native";
-import Login from "./../components/Login";
+// import { Text, View } from "react-native";
+// import Login from "./../components/Login";
+// import { auth } from "./../configs/FirebaseConfig";
+// import { useRouter } from "expo-router";
+// import { useEffect } from "react";
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Login />
-    </View>
-  );
-}
+// export default function Index() {
+//   const router = useRouter();
+//   const user = auth.currentUser;
+
+//   useEffect(() => {
+//     if (user) {
+//       router.push("/tabs/mytrip");
+//     }
+//   }, [user]);
+
+//   return (
+//     <View
+//       style={{
+//         flex: 1,
+//         justifyContent: "center",
+//         alignItems: "center",
+//       }}
+//     >
+//       <Login />
+//     </View>
+//   );
+// }
--- app/tabs/_layout.jsx
+++ app/tabs/_layout.jsx
@@ -1,6 +1,9 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
+import Entypo from "@expo/vector-icons/Entypo";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function TabLayout() {
   return (
@@ -9,9 +12,34 @@ export default function TabLayout() {
         headerShown: false,
       }}
     >
-      <Tabs.Screen name="mytrip" options={{}} />
-      <Tabs.Screen name="discover" />
-      <Tabs.Screen name="profile" />
+      <Tabs.Screen
+        name="mytrip"
+        options={{
+          tabBarLabel: "My Trip",
+          tabBarLabelStyle: { color: "black" },
+          tabBarIcon: () => (
+            <Entypo name="location-pin" size={24} color="black" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="discover"
+        options={{
+          tabBarLabel: "My Trip",
+          tabBarLabelStyle: { color: "black" },
+          tabBarIcon: () => (
+            <FontAwesome name="globe" size={24} color="black" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarLabel: "My Trip",
+          tabBarLabelStyle: { color: "black" },
+          tabBarIcon: () => <Ionicons name="people" size={24} color="black" />,
+        }}
+      />
     </Tabs>
   );
 }
