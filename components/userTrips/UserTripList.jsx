d854f443ef69d75768b7e91204a0463ba73e2e62
Jagriti Gautam <jagritigautam793@gmail.com>
2025-03-08 00:39:18 +0530

    UserTripList page added

---
+++ components/userTrips/UserTripList.jsx
@@ -0,0 +1,10 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+
+export default function UserTripList() {
+  return (
+    <View>
+      <View>{/* <Image source={"/"} /> */}</View>
+    </View>
+  );
+}
